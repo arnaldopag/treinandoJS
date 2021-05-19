d9ec4cc287ea64db4fed14f56d1e9eb55f8450a3
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-05-19 10:12:19 -0300

    treinando

---
+++ index.js
@@ -0,0 +1,20 @@
+const para = document.querySelector('p');
+
+para.addEventListener('click', atualizarNome);
+
+function atualizarNome(){
+    var nome = prompt('Insira um novo nome');
+    para.textContent = 'Jogador 1: ' + nome;
+}
+
+
+function criarParagrafo(){
+    let para = document.createElement('p');
+    para.textContent = 'Você clicou no Botão'
+    document.body.appendChild(para);
+}
+const botoes = document.querySelectorAll('button');
+
+for( var i = 0;i< botoes.length ; i++){
+    botoes[i].addEventListener('click', criarParagrafo);
+}
